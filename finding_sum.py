e6b5541bb99a21a291d352ded90db709780d3097
Pranoy Biswas Bappa <75092679+impranoybiswas@users.noreply.github.com>
2026-03-15 22:53:31 +0600

    add finding sum

---
+++ finding_sum.py
@@ -0,0 +1,6 @@
+def finding_sum(numbers, target):
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            if numbers[i] + numbers[j] == target:
+                result = [numbers[i], numbers[j]]
+                return result
